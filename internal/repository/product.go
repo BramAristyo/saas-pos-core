09e0fb5562f00ab0dc19625df1b0fdc3891c10ff
Bram Aristyo <BramAristyo@users.noreply.github.com>
2026-03-18 15:53:44 +0700

    feat: Implement ProductRepository with CRUD and pagination methods

---
+++ internal/repository/product.go
@@ -0,0 +1,66 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/BramAristyo/go-pos-mawish/internal/model"
+	"github.com/BramAristyo/go-pos-mawish/pkg/filter"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type ProductRepository struct {
+	DB *gorm.DB
+}
+
+func NewProductRepository(db *gorm.DB) *ProductRepository {
+	return &ProductRepository{
+		DB: db,
+	}
+}
+
+func (r *ProductRepository) Paginate(ctx context.Context, req filter.PaginationWithInputFilter) (int64, []model.Product, error) {
+	var products []model.Product
+	var totalRows int64
+
+	if err := r.DB.WithContext(ctx).Model(&model.Product{}).Where("is_active = ?", true).Count(&totalRows).Error; err != nil {
+		return 0, nil, err
+	}
+
+	if err := r.DB.WithContext(ctx).Where("is_active = ?", true).Offset(req.Offset()).Limit(req.PaginationInput.PageSize).Find(&products).Error; err != nil {
+		return 0, nil, err
+	}
+
+	return totalRows, products, nil
+}
+
+func (r *ProductRepository) FindById(ctx context.Context, id uuid.UUID) (*model.Product, error) {
+	var product model.Product
+
+	if err := r.DB.WithContext(ctx).Where("id = ? AND is_active = ?", id, true).Find(&product).Error; err != nil {
+		return nil, err
+	}
+
+	return &product, nil
+}
+
+func (r *ProductRepository) Store(ctx context.Context, product *model.Product) (*model.Product, error) {
+	if err := r.DB.WithContext(ctx).Create(product).Error; err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (r *ProductRepository) Update(ctx context.Context, id uuid.UUID, product *model.Product) (*model.Product, error) {
+	var existing model.Product
+	if err := r.DB.WithContext(ctx).Where("id = ? AND is_active = ?", id, true).First(&existing).Error; err != nil {
+		return nil, err
+	}
+
+	if err := r.DB.WithContext(ctx).Model(&existing).Updates(product).Error; err != nil {
+		return nil, err
+	}
+
+	return &existing, nil
+}
