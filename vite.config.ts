e9832817e418a63209bb0eee2c91ee4d04e1cfe3
baristyoo <bram.mymen@gmail.com>
2026-04-05 20:51:21 +0700

    Installing Basic Login Page using Shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
 
   return {
-    plugins: [vue(), vueDevTools()],
+    plugins: [vue(), vueDevTools(), tailwindcss()],
     build: {
       emptyOutDir: true,
       outDir: env.VITE_OUT_DIR || 'dist',
